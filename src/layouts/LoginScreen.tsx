76152e16463f599d76a827442030f60011831c39
JonathanDiazRiascos <jonathan-diaz_95@outlook.es>
2025-03-27 21:58:52 -0500

    styles

--- src/layouts/LoginScreen.tsx
+++ src/layouts/LoginScreen.tsx
@@ -1,18 +1,71 @@
-import{View, Text, TextInput} from "react-native";
+import { useState } from "react";
+import{View, Text, TextInput, StyleSheet, Alert, TouchableOpacity} from "react-native";
+import { useEffect } from "react";
+import { useNavigation } from "expo-router";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function LoginScreen(){
+    //const[email, setEmail] = useState();
+    const navigation = useNavigation();
+    useEffect(() => {
+      navigation.setOptions({headerShown:false});
+    },[navigation]);
     return (
         
             <View
-              style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+              style={styles.container}
             >
-              <Text>Login</Text>
-              <TextInput placeholder="admin@gmail.com"></TextInput>
+              <Text style={styles.title}>Sign in</Text>
+
+              <TextInput style={styles.input}
+              placeholder="admin@gmail.com" />
+
+              <TextInput style={styles.input}
+
+              placeholder="**************" 
+              secureTextEntry/>
+
+              <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
+              
             </View>
 
     );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: "center",
+    alignItems:"center",
+    backgroundColor:"#F0F0F0"
+  },
+  title:{
+    fontSize:24,
+    fontWeight:"bold",
+    marginBottom:20
+  },
+  input:{
+    width:"80%",
+    padding:10,
+    marginVertical:10,
+    backgroundColor:"while",
+    borderRadius:5,
+    borderWidth:1,
+    borderColor:"#ccc"
+  },
+  button:{
+    marginTop:20,
+    backgroundColor:"#007bff",
+    padding:10,
+    borderRadius:5,
+    width:"80%",
+    alignItems:"center"
+  },
+  buttonText:{
+    color:"white",
+    fontSize:18,
+    fontWeight:"bold"
+  }
+});
